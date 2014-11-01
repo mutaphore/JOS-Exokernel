07edd0a7e9ad7315d963941201ed723d6c7beb9c
mutaphore <deweichen47@gmail.com>
2014-10-31 21:18:55 -0400

    almost done with challenge

--- lib/readline.c
+++ lib/readline.c
@@ -1,8 +1,11 @@
 #include <inc/stdio.h>
 #include <inc/error.h>
+#include <inc/string.h>
 
 #define BUFLEN 1024
 static char buf[BUFLEN];
+
+int tab_complt(int pos);
 
 char *
 readline(const char *prompt)
@@ -38,7 +41,67 @@ readline(const char *prompt)
 				cputchar('\n');
 			buf[i] = 0;
 			return buf;
-		}
+		} else if (c == '\t') {
+         // Tab completion
+         i += tab_complt(i);
+      }
 	}
 }
 
+// Lab 5 Challenge: Tab completion
+
+#define MAX_CMDS 50
+
+static char *commands[23] = {
+   "newmotd",
+   "motd",
+   "lorem",
+   "script",
+   "testshell.key",
+   "testshell.sh",
+   "init",
+   "cat",
+   "echo",
+   "init",
+   "ls",
+   "lsfd",
+   "num",
+   "forktree",
+   "primes",
+   "primespipe",
+   "sh",
+   "testfdsharing",
+   "testkbd",
+   "testpipe",
+   "testpteshare",
+   "testshell",
+   "hello"
+};
+
+int tab_complt(int pos) {
+
+   char *start = buf + pos, *cmd;
+   int i, n, d = 0;
+
+   // Walk to the space before this command
+   while (start > buf && *start != ' ')
+      start--; 
+
+   n = buf + pos - start - 1;
+
+   // Look for a match
+   for (i = 0; n > 0 && i < 23; i++) {
+      if (strncmp(start, commands[i], n) == 0) {
+         cmd = commands[i];
+         d = strlen(cmd) - n;
+         break;
+      }
+   }
+   // Output the rest of the command
+   for (i = n + 1; d > 0 && i < strlen(cmd); i++) {
+      buf[pos++] = cmd[i];
+      cputchar(cmd[i]);
+   }
+
+   return d;
+}
