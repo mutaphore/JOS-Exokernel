c01038062d820245c5a1c7d74bafea62336adca4
mutaphore <deweichen47@gmail.com>
2014-11-03 12:06:54 -0500

    doing ex4

--- kern/e1000.c
+++ kern/e1000.c
@@ -4,7 +4,11 @@
 
 int e1000_attach(struct pci_func *pcif) {
 
+   // Enable the e1000 on PCI bus
    pci_func_enable(pcif);
+   // Map MMIO region in DRAM to physical addrs on the device
+   bar0_va = mmio_map_region(pcif->reg_base[0], pcif->reg_size[0]);
+
 
    return 1;
 }
--- kern/e1000.h
+++ kern/e1000.h
@@ -2,9 +2,12 @@
 #define JOS_KERN_E1000_H
 
 #include <kern/pci.h>
+#include <kern/pmap.h>
 
 #define E1000_VENDORID 0x8086
 #define E1000_DEVICEID 0x100E
+
+static void *bar0_va;
 
 int e1000_attach(struct pci_func *pcif);
 
